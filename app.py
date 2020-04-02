c7c0c235c159f8caeb8c38cb06715202675cb188
John Winchester <johnpaulwinchester@gmail.com>
2020-04-02 09:44:32 -0700

    Updated app to use templates. Added config files.

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask  import Flask
+# VS Code Flask Tutorials
+# Use a template to render a page
+# https://code.visualstudio.com/docs/python/tutorial-flask#_use-a-template-to-render-a-page
+
+from flask import Flask
+from flask import render_template
 from datetime import datetime
 import re
 
@@ -8,19 +13,9 @@ def home():
     return "Hello Flask"
 
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-    
-    # Filter the name argument to letters only using regular expressions.
-    # URL arguments can contain arbitrary text, so we restrict to safe
-    # characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-    
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-    
-    content = "Hello there, " + clean_name + ". It's " + formatted_now
-    return content
+def hello_there(name=None):
+     return render_template(
+         "hello_there.html",
+         name=name,
+         date=datetime.now()
+     )
